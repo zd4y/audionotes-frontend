731d89f0efb524a775ac64d75fa7f7d08735b1ee
zd4y <59004538+zd4y@users.noreply.github.com>
2023-08-21 16:44:28 -0500

    add vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import suidPlugin from "@suid/vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -12,6 +13,39 @@ export default defineConfig({
     // devtools(),
     suidPlugin(),
     solidPlugin(),
+    VitePWA({
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "AudioNotes",
+        short_name: "AudioNotes",
+        description: "Audio notes",
+        theme_color: "#eeeeee",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
   server: {
     port: 3000,
